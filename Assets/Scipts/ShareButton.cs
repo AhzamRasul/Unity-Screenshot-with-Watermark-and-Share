6f98c4a9cfb66871aeed8e3864127c55704f175e
AhzamRasul <ahzam-rasul@hotmail.com>
2023-04-14 17:17:12 +0500

    First Commit

---
+++ Assets/Scipts/ShareButton.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+public class ShareButton : MonoBehaviour
+{
+   public void ClickShare()
+   {
+        StartCoroutine(TakeScreenshotAndShare());
+   }
+
+private IEnumerator TakeScreenshotAndShare()
+{
+	yield return new WaitForEndOfFrame();
+
+	Texture2D ss = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false );
+	ss.ReadPixels( new Rect( 0, 0, Screen.width, Screen.height ), 0, 0 );
+	ss.Apply();
+
+	string filePath = Path.Combine( Application.temporaryCachePath, "shared img.png" );
+	File.WriteAllBytes( filePath, ss.EncodeToPNG() );
+
+	// To avoid memory leaks
+	Destroy( ss );
+
+	new NativeShare().AddFile( filePath )
+		.SetSubject( "arplace" ).SetText( "Sharing new content from arplace" )
+		.Share();
+
+}
+
+	public GameObject WaterMark;
+	
+	public void Trigger()
+	{
+		if(WaterMark.activeInHierarchy == false)
+		{
+			WaterMark.SetActive(true);
+		}
+		else
+		{
+			StartCoroutine(Hide());
+		}
+	}
+
+IEnumerator Hide()
+{
+	yield return new WaitForSeconds(2f);
+	WaterMark.SetActive(false);
+}	
+}
+
+
+
